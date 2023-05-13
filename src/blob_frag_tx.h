8a4e759e59dc952c6d4c13bd1d170ae8f41be329
unknown <jsphmck92@gmail.com>
2023-05-14 08:04:17 +1000

    Adding declspecs for windows build

--- src/blob_frag_tx.h
+++ src/blob_frag_tx.h
@@ -11,8 +11,20 @@ typedef struct blob_frag_tx_s
     unsigned char *p_out_buffer;
 } blob_frag_tx;
 
+#ifdef BLOB_WINDOWS_DLL
+__declspec(dllexport)
+#endif
 int
 blob_frag_tx_init(blob_frag_tx *p_blob_frag_tx, unsigned char *p_data, size_t n);
 
+#ifdef BLOB_WINDOWS_DLL
+__declspec(dllexport)
+#endif
 int
 blob_frag_tx_begin_packet(blob_frag_tx *p_blob_frag_tx, unsigned char **pp_data, size_t *p_n);
+
+#ifdef BLOB_WINDOWS_DLL
+__declspec(dllexport)
+#endif
+int
+blob_frag_tx_next_packet(blob_frag_tx *p_blob_frag_tx, unsigned char **pp_data, size_t *p_n);
