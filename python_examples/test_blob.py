adf5a1a45d39822eb5af5a5ca326f9bc56c12721
unknown <jsphmck92@gmail.com>
2023-05-16 21:59:11 +1000

    Adding blob python streaming example

--- python_examples/test_blob.py
+++ python_examples/test_blob.py
@@ -2,15 +2,21 @@ import numpy as np
 import blob.blob_write as bw
 import blob.blob_udp as bu
 import sys
-bwrite = bw.BlobWriter('test', ['jval', 'jval_squared', 'jval_cubed'])
+import time
+bwrite = bw.BlobWriter('test', ['jval', 'jval_squared', 'jval_cubed', 'abs_jval'])
 
 budp = bu.BlobUDPTx(str(sys.argv[1]), port=int(sys.argv[2]), fragment_tx_size=1400)
 i = 1
 
+N_ELEM = 100
 
 while (True):
-    bwrite.jval = np.array([i])
-    bwrite.jval_squared = np.array([i**2])
-    bwrite.jval_cubed = np.array([i**3])
+    jval = (i + 1) % N_ELEM - N_ELEM // 2
+    bwrite.jval = np.array([jval])
+    bwrite.jval_squared = np.array([jval**2]) / N_ELEM
+    bwrite.jval_cubed = np.array([jval**3]) / (N_ELEM**2)
+    bwrite.abs_jval = np.array([abs(jval)])
     data = bwrite.flush()
     budp.send(data)
+    time.sleep(0.02)
+    i = i + 1
--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setup(
     description='Blob, stream and receive arbitrary variables via udp and websockets.',
     packages=find_packages(),
     package_data={'': ['blob/*']},
+    entry_points={
+            'console_scripts': [
+                'logger = blob.logger:console',
+                'log_parser = blob.log_parse:console']
+                },
     include_package_data=True,
 )
