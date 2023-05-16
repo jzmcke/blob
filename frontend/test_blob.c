7d187f735ed93ff6661489cd84fc15d4ada9ef55
root <root@DESKTOP-SPD1KOC.localdomain>
2023-05-16 12:03:47 +1000

    Removing redundant functions

--- frontend/test_blob.c
+++ frontend/test_blob.c
@@ -3,63 +3,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "include/blob.h"
 
-#define NUM_IN_FIRST_ARRAY  (4)
-#define NUM_IN_SECOND_ARRAY (7)
-#define NUM_IN_THIRD_ARRAY (170)
 #define NELEM 100
 
-void
-func_top(void)
-{
-    int ret;
-    int a_array_first[NUM_IN_FIRST_ARRAY];
-    int a_array_second[NUM_IN_SECOND_ARRAY];
-    
-    static int start = 0;
-    BLOB_START("top");
-    for (int i=0; i<NUM_IN_FIRST_ARRAY; i++)
-    {
-        a_array_first[i] = start + i;
-    }
-    for (int i=0; i<NUM_IN_SECOND_ARRAY; i++)
-    {
-        a_array_second[i] = start + i;
-    }
-
-    ret = BLOB_INT_A("first_array", a_array_first, NUM_IN_FIRST_ARRAY);
-    assert(ret==0);
-    ret = BLOB_INT_A("second_array", a_array_second, NUM_IN_SECOND_ARRAY);
-    assert(ret==0);
-    BLOB_FLUSH();
-    start = start + 100;
-}
-
-void
-func_mid(void)
-{
-    int ret;
-    int integer_val = -1;
-    unsigned int unsigned_int_val = 4;
-    float a_array_first[NUM_IN_FIRST_ARRAY];
-
-    BLOB_START("top_second");
-    for (int i=0; i<NUM_IN_FIRST_ARRAY; i++)
-    {
-        a_array_first[i] = i+0.517;
-    }
-    BLOB_START("mid");
-    ret = BLOB_INT_A("integer_val", &integer_val, 1);
-    assert(ret ==  0);
-    ret = BLOB_UNSIGNED_INT_A("unsigned_integer_val", &unsigned_int_val, 1);
-    assert(ret ==  0);
-    ret = BLOB_FLOAT_A("float_array", a_array_first, NUM_IN_FIRST_ARRAY);
-    assert(ret ==  0);
-    BLOB_FLUSH();
-    BLOB_FLUSH();
-}
 
 int
 main(int argc, char **argv)
